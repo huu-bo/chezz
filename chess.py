ca51126a1615d71ee775b2c88939fb9dffa5eb58
huu-bo <huubhoogweg@gmail.com>
2023-06-28 11:02:31 +0200

    rewrite

--- chess.py
+++ chess.py
@@ -9,6 +9,61 @@ class NotaSyntaxException(Exception):
     pass
 
 
+class State:
+    def __init__(self, piece: str, line: bool, l_positions: list):
+        self.piece = piece
+        self.line = line
+        self.l_positions = l_positions
+
+
+class Nota_l_Position:
+    def __init__(self, direction: str, operation: str, value: int, place_value: str, line: bool):
+        if type(direction) != str:
+            raise ValueError
+        if direction not in 'xy':
+            raise ValueError
+        if type(operation) != str:
+            raise ValueError
+        if operation not in '-+=':
+            raise ValueError
+        if type(line) != bool:
+            raise ValueError
+        if type(value) != int:
+            raise ValueError
+        if type(place_value) != str:
+            raise ValueError
+        if place_value not in [' ', '*', '#',
+                               ' x', '*x', '#x',
+                               ' X', '*X', '#X']:
+            raise ValueError
+
+        self.direction = direction
+        self.operation = operation
+        self.value = value
+        self.place_value = place_value
+        self.line = line
+
+
+class Nota_r_Position:
+    def __init__(self, direction: str, operation: str, value, line: bool):
+        if type(direction) != str:
+            raise ValueError
+        if direction not in 'xy':
+            raise ValueError
+        if type(operation) != str:
+            raise ValueError
+        if operation not in '-+=':
+            raise ValueError
+        if type(line) != bool:
+            raise ValueError
+        if type(value) not in [str, int]:
+            raise ValueError
+
+        self.direction = direction
+        self.value = value
+        self.line = line
+
+
 class Board:
     def __init__(self, start=''):
         if start == '':
@@ -16,7 +71,6 @@ class Board:
                 start = file.read().replace('\n', '')
 
         print(start)
-        # TODO: nota currently does not support en passant
         # TODO: better error messages
         # TODO: make a file in which users can store Notas with names
         # TODO: parse rules
@@ -74,6 +128,54 @@ class Board:
                 x = 0
                 y += 1
 
+        i = 0
+        state = 'LHS piece'
+        piece = ''
+        rules = split[2]
+        rule = []
+        while i < len(rules):
+            c = rules[i]
+
+            print(i, c, state)
+
+            if state == 'LHS piece':
+                piece = rules[i:i+2]
+                i += 2
+                state = 'LHS comma'
+                continue
+            elif state == 'LHS comma':
+                if c == ',':
+                    state = 'LHS'
+                elif c == ':':
+                    state = 'RHS'
+                else:
+                    raise NotaSyntaxException('r,')
+            elif state == 'LHS':
+                rule = []
+
+                if c not in 'xy':
+                    raise NotaSyntaxException('rxy')
+
+                if rules[i+1] == 'l':
+                    raise NotImplementedError
+                else:
+                    if rules[i+1] not in '-+=':
+                        raise NotaSyntaxException('rLHS-+=' + rules[i+1])
+            else:
+                print(state)
+                assert False
+
+            i += 1
+
+
+def nota_rule(s: str):
+    out = []
+    split = s.split(':')
+    if len(split) != 2:
+        raise NotaSyntaxException("missing ':'")
+
+
+print(nota_rule('wp,y-1.:y-1'))
 
 # stockfish = Stockfish(depth=18, parameters={"Threads": 2, "Minimum Thinking Time": 30})
 # print(stockfish.get_fen_position())
@@ -85,4 +187,4 @@ class Board:
 #
 # print(stockfish.get_evaluation())
 
-board = Board()
+# board = Board()
